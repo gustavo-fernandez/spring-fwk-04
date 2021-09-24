450b5be03f978e411e66fb3cd91f5bb03fd4b61d
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2021-09-23 20:10:19 -0500

    Prototype Scope

--- src/main/java/com/example/springfwk04/MyService.java
+++ src/main/java/com/example/springfwk04/MyService.java
@@ -1,7 +1,18 @@
 package com.example.springfwk04;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-@Service("miServicio")
+@Service
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+// @Scope(BeanDefinition.SCOPE_SINGLETON) // default
+@Slf4j
 public class MyService {
+
+  public MyService() {
+    log.info("MyService created!");
+  }
+
 }
--- src/main/java/com/example/springfwk04/SpringFwk04Application.java
+++ src/main/java/com/example/springfwk04/SpringFwk04Application.java
@@ -1,12 +1,6 @@
 package com.example.springfwk04;
 
-import java.util.Comparator;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,23 +12,12 @@ public class SpringFwk04Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext appContext = SpringApplication.run(SpringFwk04Application.class, args);
 
-		BeanDefinition beanDefinition = BeanDefinitionBuilder
-			.genericBeanDefinition(NoEsUnBean.class)
-			.getBeanDefinition();
-		DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) appContext.getBeanFactory();
-		beanFactory.registerBeanDefinition("beanDeEjemplo", beanDefinition);
-
-		Map<String, Object> allBeans = appContext.getBeansOfType(Object.class);
-		allBeans
-			.entrySet()
-			.stream()
-			.filter(entry -> entry.getValue().getClass().getPackage().getName().startsWith("com.example.springfwk04"))
-			.sorted(Comparator.comparing(Map.Entry::getKey))
-			.forEach(entry -> log.info("beanName: {}, beanObject: {}", entry.getKey(), entry.getValue()));
-
-		NoEsUnBean beanDeEjemplo = beanFactory.getBean("beanDeEjemplo", NoEsUnBean.class);
-		log.info("Ya es un bean: {}", beanDeEjemplo);
-		log.info("Campo inyectado: {}", beanDeEjemplo.myService);
+		MyService bean = appContext.getBean(MyService.class);
+		log.info("bean: {}", bean);
+		MyService bean1 = appContext.getBean(MyService.class);
+		log.info("bean1: {}", bean1);
+		MyService bean2 = appContext.getBean(MyService.class);
+		log.info("bean2: {}", bean2);
 	}
 
 }
